f8c96ccb0971b168ee24d3c054c5298bd416859e
meydiLecuit <50368517+meydiLecuit@users.noreply.github.com>
2021-06-10 18:30:40 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'settings.dart';
-import 'listOfProducts.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+
+class Product {
+  final int index;
+  final String name;
+
+  final String merk;
+  final String prijs;
+
+  Product(this.index, this.name, this.merk, this.prijs);
+}
+
+var itemID;
 
 void main() => runApp(MyApp());
 
@@ -35,32 +49,67 @@ class Home extends StatefulWidget {
 // ignore: must_be_immutable
 class MainPage extends State<Home> {
   static File _imageFile;
+
   File imageFile() => _imageFile;
+
   _openGallary(BuildContext context) async {
     // ignore: deprecated_member_use
     var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    String dir = path.dirname(picture.path);
+    String newPath = path.join(dir, 'case01wd03id01.jpg');
+
     this.setState(() {
       _imageFile = picture;
     });
     if (_imageFile == null) {
       Navigator.pop(context);
     } else {
+      String base64Image = base64Encode(picture.readAsBytesSync());
+      String fileName = newPath.split("/").last;
+      var body = jsonEncode({"file": base64Image, "name": fileName});
+
+      await http
+          .post("http://10.0.2.2/sendPicture",
+              headers: {"Content-Type": "application/json"}, body: body)
+          .then((res) {
+        print(res.statusCode);
+      }).catchError((err) {
+        print(err);
+      });
+
       Navigator.push(
-          context, MaterialPageRoute(builder: (context) => listOfProducts()));
+          context, MaterialPageRoute(builder: (context) => listProducts()));
     }
   }
 
   _openCamera(BuildContext context) async {
     // ignore: deprecated_member_use
     var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    String dir = path.dirname(picture.path);
+    String newPath = path.join(dir, 'case01wd03id01.jpg');
+
     this.setState(() {
       _imageFile = picture;
     });
+
     if (imageFile == null) {
       Navigator.pop(context);
     } else {
+      String base64Image = base64Encode(picture.readAsBytesSync());
+      String fileName = newPath.split("/").last;
+      var body = jsonEncode({"file": base64Image, "name": fileName});
+
+      await http
+          .post("http://10.0.2.2/sendPicture",
+              headers: {"Content-Type": "application/json"}, body: body)
+          .then((res) {
+        print(res.statusCode);
+      }).catchError((err) {
+        print(err);
+      });
+
       Navigator.push(
-          context, MaterialPageRoute(builder: (context) => listOfProducts()));
+          context, MaterialPageRoute(builder: (context) => listProducts()));
     }
   }
 
@@ -116,63 +165,232 @@ class MainPage extends State<Home> {
         ),
         backgroundColor: Colors.lightBlueAccent,
         body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              Text(
-                'Welkom!',
-                style: TextStyle(
-                  fontSize: 60.0,
-                  color: Colors.red,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              Text(
-                'Om uw product te scannen gelieve op de cameraknop te drukken.',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              CircleAvatar(
-                radius:90,
-                backgroundImage: AssetImage('images/CameraCarrefour.jpg'),
-              ),
-              ButtonTheme(
-                minWidth: 100,
-                height: 100,
-                buttonColor: Colors.green,
-                child: RaisedButton(
-                  onPressed: () {
-                    _showChoiceDialog(context);
-                  },
-                  child: Icon(
-                    Icons.camera_alt_outlined,
-                    color: Colors.teal,
-                    size: 70,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Welkom!',
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    color: Colors.red,
+                    fontWeight: FontWeight.w300,
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-              child: ButtonTheme(
-                minWidth: 50,
-                height: 50,
-                buttonColor: Colors.grey,
-                child: RaisedButton(
-                  child: Icon(
-                    Icons.settings,
-                    color: Colors.teal,
-                    size: 30,
+                Text(
+                  'Om uw product te scannen gelieve op de cameraknop te drukken.',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Settings()));
-                  },
                 ),
-              )),
-            ]),
+                CircleAvatar(
+                  radius: 90,
+                  backgroundImage: AssetImage('images/fotoHome.jpg'),
+                ),
+                ButtonTheme(
+                  minWidth: 100,
+                  height: 100,
+                  buttonColor: Colors.green,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _showChoiceDialog(context);
+                    },
+                    child: Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.teal,
+                      size: 70,
+                    ),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: ButtonTheme(
+                      minWidth: 50,
+                      height: 50,
+                      buttonColor: Colors.grey,
+                      child: RaisedButton(
+                        child: Icon(
+                          Icons.settings,
+                          color: Colors.teal,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Settings()));
+                        },
+                      ),
+                    )),
+              ]),
         ));
+  }
+}
+
+class listProducts extends StatefulWidget {
+  listOfProducts createState() => listOfProducts();
+}
+
+class listOfProducts extends State<listProducts> {
+  Future<List<Product>> fetchProductList() async {
+    var response = await http.get('http://10.0.2.2/products');
+    var prediction = null;
+    await http.get("http://10.0.2.2/prediction").then((res) {
+      print(res.statusCode);
+      prediction = json.decode(res.body);
+    }).catchError((err) {
+      print(err);
+    });
+    var jsonData = json.decode(response.body);
+
+    List<Product> products = [];
+
+    for (var p in jsonData) {
+      Product product = Product(p["index"], p["name"], p["merk"], p["prijs"]);
+      if (p['name'] == prediction) {
+        products.add(product);
+      }
+    }
+
+    return products;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Carrefour'),
+        ),
+        body: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                Image.file(MainPage().imageFile(), width: 350, height: 250),
+                Text(
+                  'Kies uit tussen de gegeven product(en):',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Divider(color: Colors.black),
+                FutureBuilder(
+                    future: fetchProductList(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null) {
+                        return Container(
+                          child: Center(
+                            child: Text("Loading..."),
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              var item ="";
+                              if(snapshot.data[index].name != null) {
+                                item = snapshot.data[index].name;
+                              }
+                              if (item != "") {
+                                return RaisedButton(
+                                    child: Text(item),
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          new MaterialPageRoute(
+                                              builder: (context) => DetailPage(
+                                                  snapshot.data[index])));
+                                    });
+                              } else if (item == "") {
+                                return Container(
+                                  child: Center(
+                                    child: Text("No product found"),
+                                  ),
+                                );
+                              }
+                            });
+                      }
+                    })
+              ],
+            )));
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final Product product;
+
+  DetailPage(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          new SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: new IconButton(
+                icon: Image.asset('images/c4.png'),
+                onPressed: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyApp())),
+              ))
+        ]),
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.file(MainPage().imageFile(), width: 300, height: 300),
+            Divider(
+              color: Colors.teal.shade700,
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.title,
+                      color: Colors.lightBlue,
+                    ),
+                    title: Text(product.name,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.lightBlue,
+                        )))),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.euro,
+                      color: Colors.pink,
+                    ),
+                    title: Text(product.prijs,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.pink,
+                        )))),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.bookmark,
+                      color: Colors.purple,
+                    ),
+                    title: Text(product.merk,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.purple,
+                        ))))
+          ],
+        ),
+      ),
+    );
   }
 }
